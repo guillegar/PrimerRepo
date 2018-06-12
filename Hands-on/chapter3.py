b38b27ab6149f74f7653809d8e00c0c0814ed791
Unknown <guillermo.pondal@gmail.com>
2018-06-12 10:10:55 +0200

    tema 3
    fallo de memoria

---
+++ Hands-on/chapter3.py
@@ -0,0 +1,238 @@
+# To support both python 2 and python 3
+from __future__ import division, print_function, unicode_literals
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import f1_score
+from sklearn.neighbors import KNeighborsClassifier
+
+# Common imports
+import numpy as np
+import os
+from sklearn.datasets import fetch_mldata
+from sklearn.linear_model import SGDClassifier
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import StratifiedKFold
+from sklearn.base import clone
+# to make this notebook's output stable across runs
+np.random.seed(42)
+from sklearn.base import BaseEstimator
+from sklearn.metrics import precision_score, recall_score
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import cross_val_predict
+from sklearn.multiclass import OneVsOneClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import StandardScaler
+# To plot pretty figures
+import matplotlib
+import matplotlib.pyplot as plt
+plt.rcParams['axes.labelsize'] = 14
+plt.rcParams['xtick.labelsize'] = 12
+plt.rcParams['ytick.labelsize'] = 12
+
+# Where to save the figures
+PROJECT_ROOT_DIR = "."
+CHAPTER_ID = "classification"
+
+#guarda una imagen
+def save_fig(fig_id, tight_layout=True):
+    path = os.path.join(PROJECT_ROOT_DIR, "images", CHAPTER_ID, fig_id + ".png")
+    print("Saving figure", fig_id)
+    if tight_layout:
+        plt.tight_layout()
+    plt.savefig(path, format='png', dpi=300)
+
+
+mnist = fetch_mldata('MNIST original')
+
+X, y = mnist["data"], mnist["target"]
+X.shape
+
+some_digit = X[36000]
+some_digit_image = some_digit.reshape(28, 28)
+plt.imshow(some_digit_image, cmap = matplotlib.cm.binary,
+           interpolation="nearest")
+plt.axis("off")
+
+#_fig("some_digit_plot")
+#plt.show()
+
+#pinta un numero
+
+def plot_digit(data):
+    image = data.reshape(28, 28)
+    plt.imshow(image, cmap = matplotlib.cm.binary,
+               interpolation="nearest")
+    plt.axis("off")
+
+
+# pinta varios numeros
+def plot_digits(instances, images_per_row=10, **options):
+    size = 28
+    images_per_row = min(len(instances), images_per_row)
+    images = [instance.reshape(size, size) for instance in instances]
+    n_rows = (len(instances) - 1) // images_per_row + 1
+    row_images = []
+    n_empty = n_rows * images_per_row - len(instances)
+    images.append(np.zeros((size, size * n_empty)))
+    for row in range(n_rows):
+        rimages = images[row * images_per_row: (row + 1) * images_per_row]
+        row_images.append(np.concatenate(rimages, axis=1))
+    image = np.concatenate(row_images, axis=0)
+    plt.imshow(image, cmap=matplotlib.cm.binary, **options)
+    plt.axis("off")
+
+
+plt.figure(figsize=(9,9))
+example_images = np.r_[X[:12000:600], X[13000:30600:600], X[30600:60000:590]]
+plot_digits(example_images, images_per_row=10)
+#save_fig("more_digits_plot")
+#plt.show()
+
+X_train, X_test, y_train, y_test = X[:60000], X[60000:], y[:60000], y[60000:]
+
+shuffle_index = np.random.permutation(60000)
+X_train, y_train = X_train[shuffle_index], y_train[shuffle_index]
+
+#crear un clasificador de 5s
+#true o false si 5
+
+y_train_5 = (y_train == 5)
+y_test_5 = (y_test == 5)
+
+sgd_clf = SGDClassifier(max_iter=5, random_state=42)
+sgd_clf.fit(X_train, y_train_5)
+print(sgd_clf.predict([some_digit]))
+print(y[36000])
+
+cross_val_score(sgd_clf, X_train, y_train_5, cv=3, scoring="accuracy")
+
+skfolds = StratifiedKFold(n_splits=3, random_state=42)
+
+for train_index, test_index in skfolds.split(X_train, y_train_5):
+    clone_clf = clone(sgd_clf)
+    X_train_folds = X_train[train_index]
+    y_train_folds = (y_train_5[train_index])
+    X_test_fold = X_train[test_index]
+    y_test_fold = (y_train_5[test_index])
+
+    clone_clf.fit(X_train_folds, y_train_folds)
+    y_pred = clone_clf.predict(X_test_fold)
+    n_correct = sum(y_pred == y_test_fold)
+    print(n_correct / len(y_pred))
+
+
+class Never5Classifier(BaseEstimator):
+    def fit(self, X, y=None):
+        pass
+    def predict(self, X):
+        return np.zeros((len(X), 1), dtype=bool)
+
+
+never_5_clf = Never5Classifier()
+cross_val_score(never_5_clf, X_train, y_train_5, cv=3, scoring="accuracy")
+
+y_train_pred = cross_val_predict(sgd_clf, X_train, y_train_5, cv=3)
+print(confusion_matrix(y_train_5, y_train_pred))
+y_train_perfect_predictions = y_train_5
+confusion_matrix(y_train_5, y_train_perfect_predictions)
+precision_score(y_train_5, y_train_pred)
+
+#hasta ahora he estado haciendo una clasificacion binaria, ahora intentare hacerla completa
+#Multiclass classification
+print("sgd_clf")
+sgd_clf.fit(X_train, y_train)
+print(sgd_clf.predict([some_digit]))
+some_digit_scores = sgd_clf.decision_function([some_digit])
+print(some_digit_scores)
+print(np.argmax(some_digit_scores))
+print("ovo_clf")
+
+ovo_clf = OneVsOneClassifier(SGDClassifier(max_iter=5, random_state=42))
+ovo_clf.fit(X_train, y_train)
+print(ovo_clf.predict([some_digit]))
+
+print("forest_clf")
+
+forest_clf = RandomForestClassifier(random_state=42)
+forest_clf.fit(X_train, y_train)
+print(forest_clf.predict([some_digit]))
+print(forest_clf.predict_proba([some_digit]))
+
+print(cross_val_score(sgd_clf, X_train, y_train, cv=3, scoring="accuracy"))
+
+
+scaler = StandardScaler()
+
+X_train_scaled = scaler.fit(X_train.astype(np.float32))
+
+X_train_scaled = scaler.transform(X_train.astype(np.float32))
+print(cross_val_score(sgd_clf, X_train_scaled, y_train, cv=3, scoring="accuracy"))
+
+y_train_pred = cross_val_predict(sgd_clf, X_train_scaled, y_train, cv=3)
+conf_mx = confusion_matrix(y_train, y_train_pred)
+print(conf_mx)
+
+plt.matshow(conf_mx,cmap=plt.cm.gray)
+#plt.show()
+row_sums = conf_mx.sum(axis=1, keepdims=True)
+norm_conf_mx = conf_mx / row_sums
+
+np.fill_diagonal(norm_conf_mx, 0)
+plt.matshow(norm_conf_mx, cmap=plt.cm.gray)
+#save_fig("confusion_matrix_errors_plot", tight_layout=False)
+#plt.show()
+
+cl_a, cl_b = 8, 9
+X_aa = X_train[(y_train == cl_a) & (y_train_pred == cl_a)]
+X_ab = X_train[(y_train == cl_a) & (y_train_pred == cl_b)]
+X_ba = X_train[(y_train == cl_b) & (y_train_pred == cl_a)]
+X_bb = X_train[(y_train == cl_b) & (y_train_pred == cl_b)]
+
+plt.figure(figsize=(8,8))
+plt.subplot(221); plot_digits(X_aa[:25], images_per_row=5)
+plt.subplot(222); plot_digits(X_ab[:25], images_per_row=5)
+plt.subplot(223); plot_digits(X_ba[:25], images_per_row=5)
+plt.subplot(224); plot_digits(X_bb[:25], images_per_row=5)
+#save_fig("error_analysis_digits_plot")
+#plt.show()
+#print("superproceso1")
+#y_train_large = (y_train >= 7)
+#print("superproceso2")
+#y_train_odd = (y_train % 2 == 1)
+#print("superproceso3")
+#y_multilabel = np.c_[y_train_large, y_train_odd]
+#print("superproceso4")
+#knn_clf = KNeighborsClassifier()
+#print("superproceso5")
+#knn_clf.fit(X_train, y_multilabel)
+#print("superproceso6")
+#KNeighborsClassifier(algorithm='auto', leaf_size=30, metric='minkowski',
+#           metric_params=None, n_jobs=1, n_neighbors=5, p=2,
+#           weights='uniform')
+#print("superproceso7")
+#knn_clf.predict([some_digit])
+#print("superproceso8")
+#y_train_knn_pred = cross_val_predict(knn_clf, X_train, y_multilabel, cv=3, n_jobs=-1)
+#print("superproceso9")
+#print(f1_score(y_multilabel, y_train_knn_pred, average="macro"))
+
+print("Multioutput classification")
+
+noise = np.random.randint(0, 100, (len(X_train), 784))
+X_train_mod = X_train + noise
+noise = np.random.randint(0, 100, (len(X_test), 784))
+X_test_mod = X_test + noise
+y_train_mod = X_train
+y_test_mod = X_test
+
+some_index = 5500
+plt.subplot(121); plot_digit(X_test_mod[some_index])
+plt.subplot(122); plot_digit(y_test_mod[some_index])
+save_fig("noisy_digit_example_plot")
+plt.show()
+
+knn_clf = KNeighborsClassifier()
+knn_clf.fit(np.asarray(X_train_mod), np.asarray(y_train_mod))
+clean_digit = knn_clf.predict([X_test_mod[some_index]])
+plot_digit(clean_digit)
+save_fig("cleaned_digit_example_plot")
+plt.show()
