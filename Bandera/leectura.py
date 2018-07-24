794ae019de8ce63aabc7c8793b857e0b1f2bc6ba
Guillermo García <guillermo.pondal@gmail.com>
2018-07-24 13:58:49 +0200

    antes de vacaciones

--- Bandera/leectura.py
+++ Bandera/leectura.py
@@ -13,6 +13,7 @@ import pylab as pl
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.spatial.distance import cdist
 
 from sklearn.cluster import KMeans
 from sklearn.metrics import pairwise_distances_argmin_min
@@ -28,7 +29,8 @@ df_hints = pd.read_sql("SELECT * FROM hints_log", con=conn)
 df_scores = pd.read_sql("SELECT * FROM scores_log", con=conn)
 df_failures = pd.read_sql("SELECT * FROM failures_log", con=conn)
 df_teams = pd.read_sql("SELECT id, points, last_score,created_ts FROM teams", con=conn)
-
+df_teamsData = pd.read_sql("SELECT team_id,name FROM teams_data", con=conn)
+df_teamsData=df_teamsData[df_teamsData.name != ""]
 
 
 
@@ -49,20 +51,23 @@ def scores_at_first(df_failures,df_scores,df_result):
 
 
 def tiempo_medio(df_actions,df_result):
-
+    df_actions =df_actions.sort_values('ts')
     for i, row in df_actions.iterrows():
         time=row['ts']
-        level=row['level_id']
         team=row['team_id']
         #esto es un select
         und=df_result.loc[(df_result['team_id']==(team))]
 
-
-        if((time-und.ultimo_intento[0])<144000):
+        if((time-und['ultimo_intento'].iloc[0])<144000):
             df_result.loc[df_result.team_id == team,'media_intentos']+=time-und['ultimo_intento']
             df_result.loc[df_result.team_id == team,'numero_intentos']+=1
+        else:
+            df_result.loc[df_result.team_id == team,'numero_intentos']+=1
+
+# anadir aqui el numero de sesiones
 
         df_result.loc[df_result.team_id == team,'ultimo_intento']=time
+
 
 
 def ts_a_segundos(df,column_name):
@@ -84,11 +89,12 @@ ts_a_segundos(df_teams ,'created_ts')
 df_teams['total_time']=df_teams['last_score']-df_teams['created_ts']
 # elimina fecha de creacion y la de la ultima actividad para dejar solo el tiempo total, evitar datos relacionados
 df_teams=df_teams.drop(['last_score','created_ts'],axis=1)
-
 df_actions = pd.merge(df_failures, df_scores,how='outer')
+df_actions = pd.merge(df_actions, df_hints,how='outer')
+
 df_actions=df_actions.drop(['flag','points','type'],axis=1)
 
-
+team_countPlayers = df_teamsData.groupby('team_id')['team_id'].count().reset_index(name ='total_players')
 
 team_countFails = df_failures.groupby('team_id')['team_id'].count().reset_index(name ='total_fails')
 team_countHints = df_hints.groupby('team_id')['team_id'].count().reset_index(name ='total_hints')
@@ -96,36 +102,64 @@ team_countScores = df_scores.groupby('team_id')['team_id'].count().reset_index(n
 
 df_result = pd.merge(team_countHints, team_countFails,on='team_id',how='outer')
 df_result = pd.merge(df_result, team_countScores,on='team_id',how='outer')
+df_result = pd.merge(df_result, team_countPlayers,on='team_id',how='outer')
+
 df_teams = df_teams.rename(columns={'id': 'team_id'})
 df_result = pd.merge(df_result, df_teams,on='team_id',how='outer')
 df_result["scores_at_first"]=0
 df_result["numero_intentos"]=0
+df_result["sesiones"]=0
 df_result["ultimo_intento"]=0
 df_result["media_intentos"]=0
 scores_at_first(df_failures,df_scores,df_result)
 tiempo_medio(df_actions,df_result)
+df_result['media_intentos']=df_result['media_intentos']/df_result['numero_intentos']
 
 
 
 
-
-
-
-df_result=df_result.drop(['team_id'],axis=1)
 
 df_result=df_result.fillna(0)
+df_result=df_result[df_result.total_scores != 0]
+df_result_data=df_result.copy()
+df_teams=df_result.team_id
+df_result=df_result.drop(['numero_intentos','team_id',"ultimo_intento","total_time"],axis=1)
+
+# Standardize the data attributes for the Iris dataset.
+from sklearn.datasets import load_iris
+from sklearn import preprocessing
+
+df_result = preprocessing.normalize(df_result)
+
+
+
 pca = PCA(n_components=2).fit(df_result)
 pca_2d = pca.transform(df_result)
-
+print pca.get_precision()
 print df_result
 
 print pca
-kmeans = KMeans(n_clusters=3, random_state=0)
+kmeans = KMeans(n_clusters=4, random_state=0)
 clusters = kmeans.fit(df_result)
 print kmeans.cluster_centers_.shape
 pl.scatter(pca_2d[:, 0], pca_2d[:, 1])
 pl.figure('K-means with 3 clusters')
 pl.scatter(pca_2d[:, 0], pca_2d[:, 1], c=kmeans.labels_)
+for i, txt in enumerate(df_teams):
+    pl.annotate(txt,  xy=( pca_2d[i, 0],  pca_2d[i, 1]))
+print( kmeans.labels_)
 pl.show()
+
+Nc = range(1, 20)
+kmeans = [KMeans(n_clusters=i) for i in Nc]
+kmeans
+score = [kmeans[i].fit(df_result).score(df_result) for i in range(len(kmeans))]
+score
+pl.plot(Nc,score)
+pl.xlabel('Number of   Clusters')
+pl.ylabel('Score')
+pl.title('Elbow Curve')
+pl.show()
+
 cur.close()
 conn.close()
